9a71484205b5b9680543cf6f646d78ef1f0bcd30
jcapsmain <johncapoy1@gmail.com>
2024-12-20 00:05:14 +0800

    chapter 13

--- nextjs-dashboard/app/ui/invoices/edit-form.tsx
+++ nextjs-dashboard/app/ui/invoices/edit-form.tsx
@@ -19,7 +19,7 @@ export default function EditInvoiceForm({
   customers: CustomerField[];
 }) {
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
-  return <form action={updateInvoiceWithId}></form>;(
+  return (
     <form>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
