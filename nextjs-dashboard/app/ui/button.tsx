f002a38e3917972cb82817d0f23409ffe8894d9b
jcapsmain <johncapoy1@gmail.com>
2024-12-19 23:53:05 +0800

    chapter 12

--- nextjs-dashboard/app/ui/button.tsx
+++ nextjs-dashboard/app/ui/button.tsx
@@ -1,7 +1,20 @@
 import clsx from 'clsx';
+import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
+}
+
+export function UpdateInvoice({ id }: { id: string }) {
+  return (
+    <Link
+      href={`/dashboard/invoices/${id}/edit`}
+      className="rounded-md border p-2 hover:bg-gray-100"
+    >
+      <PencilIcon className="w-5" />
+    </Link>
+  );
 }
 
 export function Button({ children, className, ...rest }: ButtonProps) {
