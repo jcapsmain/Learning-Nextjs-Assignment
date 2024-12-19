dbf3a4e3100acd7d49d66b4325292bb29fff0603
jcapsmain <johncapoy1@gmail.com>
2024-12-19 18:03:02 +0800

    chapter 7

--- nextjs-dashboard/app/query/route.ts
+++ nextjs-dashboard/app/query/route.ts
@@ -4,10 +4,10 @@
 
  async function listInvoices() {
  	const data = await client.sql`
-     SELECT invoices.amount, customers.name
-     FROM invoices
-     JOIN customers ON invoices.customer_id = customers.id
-     WHERE invoices.amount = 666;
+      SELECT invoices.amount, customers.name
+      FROM invoices
+      JOIN customers ON invoices.customer_id = customers.id
+      WHERE invoices.amount = 666;
    `;
 
  	return data.rows;
